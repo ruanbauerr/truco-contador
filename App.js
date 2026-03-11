5c903b18b4d49101b75c5c3132e52c82c9ab58dc
ruanbauerr <ruanbauer4@gmail.com>
2026-03-10 23:30:27 -0300

    feat: adiciona contador simples com incremento e decremento

--- App.js
+++ App.js
@@ -1,35 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [contador, setContador] = useState(0);
 
-  const[contador, setContador] = useState(0);
-
-  function diminuirContagem(){
-    if (contador > 0){
-    setContador(contador - 1);
+  function aumentarContagem() {
+    if (contador <= 11) {
+      setContador(contador + 1);
     }
-    
   }
 
-return (
+  function diminuirContagem() {
+    if (contador > 0) {
+      setContador(contador - 1);
+    }
+  }
+
+  return (
     <View style={styles.container}>
-      <Text>Contador de valores: </Text>
-      
-      
-      <Text>{contador}</Text>
-      
-      <View style = {{flexDirection:'row', marginTop: 10, marginHorizontal: 30}}>
-      <Button 
-        title='Aumentar' onPress={() => setContador(contador + 1)} 
-      />
-      <Button 
-        title='Diminuir' onPress={diminuirContagem} 
-      />
+      <Text style={{ fontSize: 40 }}>Marcador</Text>
+
+      <Text style={{ fontSize: 70 }}>{contador}</Text>
+
+      <View
+        style={{ flexDirection: "row", marginTop: 10, marginHorizontal: 30 }}
+      >
+        <View style={{ width: 70, marginRight: 10 }}>
+          <Button title="+" color="#41e405" onPress={aumentarContagem} />
+        </View>
+        <View style= {{width: 70, marginRight:10}}>
+          <Button title="-" color="#f50e0e" onPress={diminuirContagem} />
+        </View>
       </View>
-      
+
       <StatusBar style="auto" />
     </View>
   );
@@ -38,8 +42,8 @@ return (
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
